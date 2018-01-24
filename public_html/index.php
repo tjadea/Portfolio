55255259e6ea9e619737faefdec93d1834ad8d7f
Tamara Argent <tjargent@outlook.com>
2018-01-24 18:19:23 +0000

    Update and backup of personal portfolio website public_html files

---
+++ public_html/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Tamara Argent | Portfolio</title>
+	<script src="../js/jquery.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="../css/font-awesome/css/fontawesome-all.css">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/stylesheet.css">
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-default navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainNavbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					
+				</div>
+				<div class="collapse navbar-collapse" id="mainNavbar">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#portfolio-content">Portfolio</a></li>
+						<li><a href="#details">About</a></li>
+						<li><a href="#footer">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<div class="container-fluid">
+	<section id="info">
+		<h1>Hi, I'm Tamara
+		<br>I am a Junior Front End Web Developer based in Haverhill, Suffolk</h1>
+		<br><a href="#details"><span class="glyphicon glyphicon-circle-arrow-down"></span></a>
+	</section>
+	<section id="details">
+	    <h2>About Me</h2><br>
+	    <p>I have a graduate background in Computer Security and Forensics and with previous experience in the Education sector, I am now branching out to another love of mine - Website Development!</p>
+	    <p>I have casually tweaked and coded on and off for several years, but over the last year I have aspired to pursue a career in something I have always enjoyed.</p>
+	    <p>I am always adapting and learning an array of new technologies and frameworks. If you view my skillset below you will have a taster of the frameworks and languages I have looked into as well as my current challenges and aspirations for the future.</p>
+	    <p>I am learning in a Linux (Mint) environment and have side personal projects available on my Github.
+	    <br><button class="btn-ask">E-Mail Me</button</p>
+	</section>
+	<h2>Recent  Works</h2>
+	    <br><p>...</p>
+	    <br>
+	<section class="portfolio-content" id="portfolio-content">
+			 <div class="thumbnail photos">
+			 	<a href="portfolio/photography.html">
+			 	<br><h3>PHOTOGRAPHY</h3>
+			 	</a>
+			</div>
+			 <div class="thumbnail HH">
+			    <a href="/portfolio/haverhill.html">
+			 	<br><h3>HAVERHILL</h3>
+			    </a>
+			</div>
+	</section>
+	<section id="skills">
+	    <h2>Skills</h2><br>
+	    <p>HTML | CSS | Bootstrap | Javascript</p>
+	</section>
+	</div>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109155057-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-109155057-1');
+	</script>
+	<script src="../js/fluidscroll.js"></script>
+	<footer class="footer" id="footer">
+		<ul class="container-fluid social-footer">
+			<li><a href="" target="_blank"><i class="fab fa-behance fa-2x"></i>
+				<span class="sr-only">Behance</span></a>
+			</li>
+			<li><a href="" target="_blank"><i class="fab fa-instagram fa-2x"></i>
+				<span class="sr-only">Instagram</span></a>
+			</li>
+			<li><a href="" target="_blank"><i class="fab fa-codepen fa-2x"></i>
+				<span class="sr-only">Codepen</span></a>
+			</li>
+			<li><a href="" target="_blank"><i class="fab fa-git fa-2x"></i>
+				<span class="sr-only">Github</span></a>
+			</li>
+			<li><a href="" target="_blank"><i class="fab fa-linkedin fa-2x"></i>
+				<span class="sr-only">LinkedIn</span></a>
+			</li>
+			<p>Copyright &copy; 2017 - 2018 Tamara Argent</p>
+		</ul>
+	</footer>
+</body>
+</html>
